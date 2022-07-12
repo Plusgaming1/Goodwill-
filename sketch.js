4b0ee48d3c527b5ac3521dd3aa5f040e35af3071
Plusgaming1 <76749385+Plusgaming1@users.noreply.github.com>
2022-07-12 12:23:43 +0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  resizeCanvas(windowWidth, windowHeight)
+  background(51);
+  fill(255);
+  textSize(100);
+  text("Hi", width/2, 20, 50);
+}
